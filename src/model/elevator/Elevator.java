f5216b672936b8c15f32a80224c4465d6d0fa724
elnaznosrati <elnaznosrati39@gmail.com>
2026-06-18 21:14:58 +0330

    feat: implement elevator movement and pickup logic

--- src/model/elevator/Elevator.java
+++ src/model/elevator/Elevator.java
@@ -1,10 +1,10 @@
 package model.elevator;
 
 import model.building.Building;
-import model.building.PassengerQueue;
 import model.fairness.FairnessStrategy;
 import model.passenger.Passenger;
 import model.repair.RepairCenter;
+import model.simulation.SimulationManager;
 
 import java.util.Random;
 
@@ -73,6 +73,61 @@ public abstract class Elevator implements Runnable {
 
             System.out.println(getType() + " elevator " + id + " stopped!");
         }
+    }
+
+    private void servePassenger(Passenger passenger) throws InterruptedException {
+        double passengerWeight = passenger.getTotalWeight();
+        SimulationManager.getInstance().enterElevatorWeight(passengerWeight);
+
+        currentPassenger = passenger;
+        passenger.markRiding(id);
+        System.out.println(passenger.getRole() + " entered " + getType() + " elevator " + id + " on floor " + currentFloor);
+
+        moveTo(passenger.getTargetFloor());
+
+        System.out.println(passenger.getRole() + " left elevator " + id + " on floor " + currentFloor);
+        passenger.notifyArrived(currentFloor);
+        currentPassenger = null;
+
+        SimulationManager.getInstance().leaveElevatorWeight(passengerWeight);
+    }
+
+    private void moveTo(int targetFloor) throws InterruptedException {
+        while (running && !broken && currentFloor != targetFloor) {
+            if (currentFloor < targetFloor) {
+                currentFloor++;
+            } else {
+                currentFloor--;
+            }
+            SimulationManager.getInstance().addTravelTime(TRAVEL_TIME_PER_FLOOR);
+            System.out.println(getType() + " elevator " + id + " is on floor " + currentFloor);
+            Thread.sleep(TRAVEL_TIME_PER_FLOOR);
+            checkBreakdown();
+        }
+    }
+
+    private void moveOneFloor() throws InterruptedException {
+        int lastFloor = building.getFloorCount() - 1;
+        if (lastFloor <= 0) {
+            Thread.sleep(300);
+            return;
+        }
+
+        if (currentFloor >= lastFloor) {
+            goingUp = false;
+        } else if (currentFloor <= 0) {
+            goingUp = true;
+        }
+
+        if (goingUp) {
+            currentFloor++;
+        } else {
+            currentFloor--;
+        }
+
+        SimulationManager.getInstance().addTravelTime(TRAVEL_TIME_PER_FLOOR);
+        System.out.println(getType() + " elevator " + id + " is on floor " + currentFloor);
+        Thread.sleep(TRAVEL_TIME_PER_FLOOR);
     }
 
     private void checkBreakdown() {
