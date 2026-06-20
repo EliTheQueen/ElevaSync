3f2a50ca8bdd1aad6647f410a7f6a8b7bec67a55
elnaznosrati <elnaznosrati39@gmail.com>
2026-06-20 23:01:11 +0330

    feat: implement repair center queue

--- src/model/repair/RepairCenter.java
+++ src/model/repair/RepairCenter.java
@@ -1,4 +1,34 @@
 package model.repair;
 
+import model.elevator.Elevator;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class RepairCenter {
+
+    private final Queue<Elevator> brokenElevators = new LinkedList<>();
+    private boolean running = true;
+
+    public synchronized void reportBrokenElevator(Elevator elevator) {
+        brokenElevators.add(elevator);
+        System.out.println("Repair center received broken elevator" + elevator.getId());
+        notifyAll();
+    }
+
+    public synchronized Elevator takeBrokenElevator() throws InterruptedException {
+        while (running && brokenElevators.isEmpty()) {
+            wait();
+        }
+        if (!running) {
+            return null;
+        }
+
+        return brokenElevators.poll();
+    }
+
+    public synchronized void shutdown() {
+        running = false;
+        notifyAll();
+    }
 }
